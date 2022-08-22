3565ac3f7424bb0163ac0b935a745fa9e82f9720
Suhas Nadarge <nadargesuhas@gmail.com>
2022-08-22 10:44:00 +0100

    seat conflicts while booking

--- booking-api/web-socket/index.js
+++ booking-api/web-socket/index.js
@@ -10,20 +10,44 @@
 
 
 module.exports = (io) => {
-  
+ 
+
+  let usersList = []
+  let allTickets = []
+  const msgList = []
 io.on('connection', (socket) => {
   
-    console.log('a user connected');
+    console.log('a user connected-1');
     
     socket.on('disconnect', () => {
       console.log('user disconnected');
+     usersList = [];
+
+
     });
     
-    socket.on('my broadcast', (msg) => {
-      console.log('message: ' + msg);
-      io.emit('my broadcast', `server: ${msg}`);
+    socket.on('my broadcast', (obj) => {
+      var userTempObj = {
+        id:'',
+        seatList:[]
+      }
+      if(!usersList.includes(obj.id)){
 
+        usersList.push(obj.id)
+        userTempObj.id = obj.id,
+        userTempObj.seatList.push(obj.seatObj.seatLabel) 
+        allTickets.push(userTempObj);
+      } else {
+        allTickets.map(elr=> elr.id === obj.id ? elr.seatList.push(obj.seatObj.seatLabel): elr.seatList = elr.seatList )
+      }
+      console.log('Hello from Angular: ' + JSON.stringify(obj));
+      msgList.push(obj.id)
+      console.log('----)'+ usersList,allTickets);
+
+      io.emit('my broadcast', usersList,allTickets);
     });
   });
     }
+
+    
  
